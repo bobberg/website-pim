c1522270f7526e3d9067e85110d5f1b7e03ab27b
Bob van den Berg <bobberg90@gmail.com>
2022-01-17 22:51:42 +0100

    made http into https for vids

--- src/components/VideoSection.tsx
+++ src/components/VideoSection.tsx
@@ -4,14 +4,14 @@ const VideoSection = (image: any) => {
   const VimeoLinks = [
     {
       key: 1,
-      imgsrc: "http://i.vimeocdn.com/video/268038873_640.jpg",
+      imgsrc: "https://i.vimeocdn.com/video/268038873_640.jpg",
       src: "http://player.vimeo.com/video/38902000",
       title: "Mumbai Crawford Market",
       alt: "",
     },
     {
       key: 2,
-      imgsrc: "http://i.vimeocdn.com/video/228068936_640.jpg",
+      imgsrc: "https://i.vimeocdn.com/video/228068936_640.jpg",
       src: "http://player.vimeo.com/video/33585155",
       title: "Stand Up Inspiration",
       text: "Stand-up Inspiration Talk over aandachtig kijken. Op uitnodiging van Martijn Aslander.",
@@ -19,7 +19,7 @@ const VideoSection = (image: any) => {
     },
     {
       key: 3,
-      imgsrc: "http://i.vimeocdn.com/video/147977975_640.jpg",
+      imgsrc: "https://i.vimeocdn.com/video/147977975_640.jpg",
       src: "http://player.vimeo.com/video/22828240",
       title: "Fismarket early morning Dubai",
       text: "The oldest business on earth. Passionate and trustful eyes from traders of the Middle East.",
@@ -27,7 +27,7 @@ const VideoSection = (image: any) => {
     },
     {
       key: 4,
-      imgsrc: "http://i.vimeocdn.com/video/132757816_640.jpg",
+      imgsrc: "https://i.vimeocdn.com/video/132757816_640.jpg",
       src: "http://player.vimeo.com/video/20731140",
       title: "Old Shanghai Foodcourt in its broadest sense.",
       alt: "",
